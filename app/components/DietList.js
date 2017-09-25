0dea15d6adbdee1b2199157a7395defe0e1f31dc
Pierre Killy <pierre@kwali.to>
2017-09-25 11:50:31 +0200

    chore(replace deprecated usage of prop-types from react for its preferred dedicated package)

--- app/components/DietList.js
+++ app/components/DietList.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { List, ListSubHeader } from 'react-toolbox/lib/list';
 
 import DietListItem from './DietListItem';
